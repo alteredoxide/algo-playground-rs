e6b3b69b97635c6b5952cd654f4fe514da1d85ba
alteredoxide <me@alteredoxide.com>
2023-11-05 11:43:23 -0800

    add ptr impls
     Added an additional second-pass implementations of insertion sort that
     uses manually implemented element swapping:
     - the current implementation relies on the swap using element mut raw pointers
     - there is also a second swap function that uses a mut raw pointer to
       the slice itself, with offsets

--- src/sorting/insertion/second.rs
+++ src/sorting/insertion/second.rs
@@ -21,6 +21,42 @@ fn sort_inplace2<T: PartialOrd>(values: &mut [T]) {
 }
 
 
+/// Manual implementation of swapping slice elements using pointers of the elements.
+/// NOTE: This function uses unsafe code, but still meets guaranteeds. The arg type for
+/// `values` could be an immutable ref if this function were itself labeled as unsafe,
+/// thereby pushing responsibility onto the caller to guarantee correctness.
+fn swap_elements<T: PartialOrd>(values: &mut [T], i: usize, j: usize) {
+    let pa = std::ptr::addr_of_mut!(values[i]);
+    let pb = std::ptr::addr_of_mut!(values[j]);
+    unsafe {
+        std::ptr::swap(pa, pb);
+    }
+}
+
+
+/// Manual implementation of swapping slice elements using slice pointer with offset.
+/// NOTE: Uses unsafe code; similar to `swap_elements()`.
+fn swap_elements_offset<T: PartialOrd>(values: &mut [T], i: usize, j: usize) {
+    if i >= values.len() || j >= values.len() {
+        panic!("index out of bounds");
+    }
+    unsafe {
+        let pa = values.as_ptr().offset(i as isize) as *mut T;
+        let pb = values.as_ptr().offset(j as isize) as *mut T;
+        std::ptr::swap(pa, pb);
+    }
+}
+
+
+fn sort_inplace_ptrs<T: PartialOrd>(values: &mut [T]) {
+    for i in 1..values.len() {
+        for j in (1..=i).rev() {
+            if values[j-1] > values[j] { swap_elements_offset(values, j-1, j) }
+        }
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
 
@@ -56,6 +92,24 @@ mod tests {
         ];
         for (case, expect) in cases.iter_mut().zip(&expected) {
             super::sort_inplace2(case);
+            assert_eq!(case, expect);
+        }
+    }
+
+    #[test]
+    fn sort_inplace_ptrs() {
+        let mut cases: Vec<Vec<i32>> = vec![
+            vec![0, 2, 1, 4, 7, 3],
+            vec![2, -1, 5, 2, 9],
+            vec![0, 1, 2, 4, 3, 0],
+        ];
+        let expected: Vec<Vec<i32>> = vec![
+            vec![0, 1, 2, 3, 4, 7],
+            vec![-1, 2, 2, 5, 9],
+            vec![0, 0, 1, 2, 3, 4],
+        ];
+        for (case, expect) in cases.iter_mut().zip(&expected) {
+            super::sort_inplace_ptrs(case);
             assert_eq!(case, expect);
         }
     }
